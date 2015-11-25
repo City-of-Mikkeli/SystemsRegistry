87f55ff9bd3eb91fd9f78c5b42559ff551ba3103
Heikki Kurhinen <heikki.kurhinen@otavanopisto.fi>
2015-11-25 15:33:54 +0200

    ** Added script to handle rendering modal windows

---
+++ public/script/modal.js
@@ -0,0 +1,28 @@
+(function($){
+	'use strict';
+	
+	function getFormHtml(itemType, cb){
+		$.ajax({
+			url: SERVER_ROOT+'template/'+itemType,
+			dataType: 'html',
+			success: cb
+		})
+	}
+	
+	$('#new-item-modal.modal').on('show.bs.modal', function (event) {
+	  var button = $(event.relatedTarget);
+	  $(this).find('.modal-form-container').remove();
+	  $(this).find('.loader').show();
+	   $(this).find('.modal-title').text(button.attr('data-modal-title'));
+	  var itemType = button.attr('data-item-type');
+	  getFormHtml(itemType, $.proxy(function(form){
+		  var wrappedForm = $('<div>')
+		    .addClass('modal-form-container')
+			.append(form);
+		  $(this).find('.loader').hide();
+		  $(this).find('.modal-body').append(wrappedForm);
+	  }, this));
+	});
+
+	
+})($);
