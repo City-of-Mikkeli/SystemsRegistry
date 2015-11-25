edbdb0c48cd0ca36e5722fd96668d2b4dcf39610
Heikki Kurhinen <heikki.kurhinen@otavanopisto.fi>
2015-11-25 15:35:03 +0200

    ** Added route to server side ajax template

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,14 @@
 module.exports = function(app, root){
 	
+	// Navigation
 	app.get(root, function(req, res){
 		res.render('index', {root: root});
 	});
-
+	
+	// API
+	
+	// Ajax-templates
+	app.get(root+'template/application', function(req, res){
+		res.render('new-application-form', {root: root});
+	});
 };
