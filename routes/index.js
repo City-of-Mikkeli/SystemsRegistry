1fb12c885d38c2a749527b2f04d0e3abe4b9e6db
Heikki Kurhinen <heikki.kurhinen@otavanopisto.fi>
2015-11-26 14:33:18 +0200

    ** Added application rest api

--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,5 @@
+var application = require('./application');
+
 module.exports = function(app, root){
 	
 	// Navigation
@@ -6,9 +8,26 @@ module.exports = function(app, root){
 	});
 	
 	// API
+	app.post(root+'api/application', application.create);
+	app.put(root+'api/application/:id', application.update);
+	app.get(root+'api/application', application.list);
+	app.get(root+'api/application/:id', application.get);
+	app.delete(root+'api/application/:id', application.remove);
 	
 	// Ajax-templates
 	app.get(root+'template/application', function(req, res){
 		res.render('new-application-form', {root: root});
+	});
+	
+	app.get(root+'template/server', function(req, res){
+		res.render('new-server-form', {root: root});
+	});
+	
+	app.get(root+'template/contract', function(req, res){
+		res.render('new-contract-form', {root: root});
+	});
+	
+	app.get(root+'template/integration', function(req, res){
+		res.render('new-integration-form', {root: root});
 	});
 };
