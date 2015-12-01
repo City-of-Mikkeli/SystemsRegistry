0237c0ea9ce373807cfabed4738fda40df5ff0d4
Heikki Kurhinen <heikki.kurhinen@otavanopisto.fi>
2015-12-01 15:36:14 +0200

    ** changed session secret

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
 	port: 3000,
 	server_root: '/',
-	session_secret: 'ofdhij1i9o23ij4e1290h',
+	session_secret: 'your secret here',
 	database: {
 		host: 'localhost',
 		table: 'systemsregistry'
