634fd7d5fed78e5164772d019acf9b69931ee136
Heikki Kurhinen <heikki.kurhinen@otavanopisto.fi>
2015-12-01 15:30:12 +0200

    ** Improved server view script

--- public/script/server-view.js
+++ public/script/server-view.js
@@ -8,9 +8,19 @@
 		});
 	}
 	
+	function getContractInfo(element){
+		var id = $(element).text();
+		$.getJSON(SERVER_ROOT+'api/contract/'+id, function(contract){
+			$(element).text(contract.name);
+		});
+	}
+	
 	$(document).ready(function(){
 		$('.server-app').each(function(){
 			getAppInfo(this);	
+		});
+		$('.server-contract').each(function(){
+			getContractInfo(this);	
 		});
 	});
 	
